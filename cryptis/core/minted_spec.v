f464011227e04ff9ab039940290573cf1cf84f15
Chandradeep Dey <codesigning@chandradeepdey.com>
2026-05-18 22:28:39 -0400

    minted_spec builds again

--- cryptis/core/minted_spec.v
+++ cryptis/core/minted_spec.v
@@ -79,19 +79,47 @@ Qed.
 Lemma minted_spec_THash t : minted_spec (THash t) ⊣⊢ minted_spec t.
 Proof. by rewrite unlock nonces_of_term_unseal /=. Qed.
 
+Lemma minted_TInv t : minted_spec (TInv t) ⊣⊢ minted_spec t.
+Proof. by rewrite unlock nonces_of_termE. Qed.
+
 Lemma minted_spec_TExpN t ts :
+  ~ is_exp t -> invs_canceled ts ->
   minted_spec (TExpN t ts) ⊣⊢ minted_spec t ∧ [∗ list] t' ∈ ts, minted_spec t'.
 Proof.
-rewrite unlock nonces_of_term_TExpN big_sepS_union_pers.
+move => /negb_True ??.
+rewrite unlock nonces_of_term_TExpN // cancel_exps_canceled // big_sepS_union_pers.
 by rewrite big_sepS_union_list_pers big_sepL_fmap.
 Qed.
 
+Lemma minted_spec_base_exps t :
+  minted_spec t ⊣⊢ minted_spec (base t) ∧ [∗ list] t' ∈ exps t, minted_spec t'.
+Proof. rewrite -{1}[t]base_expsK minted_spec_TExpN //; exact: invs_canceled_exps. Qed.
+
+Lemma all_minted_spec_TExpN t ts :
+  minted_spec t ∧ ([∗ list] t' ∈ ts, minted_spec t') ⊢ minted_spec (TExpN t ts).
+Proof.
+rewrite unlock !big_sepS_forall.
+iIntros "[Ht Hts]" (l) "%l_in".
+have /elem_of_subseteq in_nonces := nonces_of_term_TExpN_subseteq t ts.
+move: l_in => /(in_nonces l). rewrite elem_of_union elem_of_union_list.
+case => [?|]; first by iApply "Ht".
+case => _ [] /list_elem_of_fmap [] t' [] -> ??.
+rewrite big_sepL_elem_of // big_sepS_forall.
+by iApply "Hts".
+Qed.
+
 Lemma minted_spec_TExp t1 t2 :
+  ~ is_exp t1 ->
   minted_spec (TExp t1 t2) ⊣⊢ minted_spec t1 ∧ minted_spec t2.
 Proof.
-rewrite unlock nonces_of_term_TExpN big_sepS_union_pers.
-by rewrite /= union_empty_r_L.
+move => /negb_True ?.
+rewrite unlock nonces_of_term_TExpN // cancel_exps1.
+by rewrite big_sepS_union_pers /= union_empty_r_L.
 Qed.
+
+Lemma all_minted_spec_TExp t1 t2 :
+  minted_spec t1 ∧ minted_spec t2 ⊢ minted_spec (TExp t1 t2).
+Proof. by iIntros; iApply all_minted_spec_TExpN; rewrite big_sepL_singleton. Qed.
 
 Lemma minted_spec_nonces_of_term t :
   minted_spec t ⊣⊢ [∗ set] a ∈ nonces_of_term t, minted_spec (TNonce a).
